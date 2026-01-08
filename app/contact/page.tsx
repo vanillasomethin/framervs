99e96ee383b0c93a82d9ab657844cacf502114d9
Vanilla & Somethin <109773901+vanillasomethin@users.noreply.github.com>
2026-01-08 11:57:21 +0300

    Remove React/Next scaffolding in favor of static HTML

--- app/contact/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LegacyPage } from "@/components/legacy-page";
-
-export default function ContactPage() {
-  return <LegacyPage sourcePath="contact/index.html" />;
-}
--- app/page-1/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LegacyPage } from "@/components/legacy-page";
-
-export default function ExtraPage() {
-  return <LegacyPage sourcePath="page(1).html" />;
-}
